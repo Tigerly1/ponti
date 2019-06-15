531a2da87f4fd6b4c9907bb15b33e908280a52d3
Tigerly1 <tigerlymad@gmail.com>
2019-06-15 12:35:12 +0200

    changed

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express")
 var app = express()
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 var bodyParser = require("body-parser")

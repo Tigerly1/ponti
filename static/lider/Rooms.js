6424d8370072959854f6fe265da79c25950c2ee5
Tigerly1 <tigerlymad@gmail.com>
2019-08-08 19:22:00 +0200

    'okej'

--- static/lider/Rooms.js
+++ static/lider/Rooms.js
@@ -1,6 +1,6 @@
 class Rooms {
     constructor() {
-        var host = "localhost"
+        var host = "ponti"
         this.Lider = this.io(host)
         if (document.cookie != "")
             if (document.cookie.split("=")[1].split("")[4] == "L") this.createRoom(document.cookie.substring(5, 9))

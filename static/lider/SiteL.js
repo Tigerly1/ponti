4f826dde2b0caa44bb181a47b7d0b00bae14b1ec
Tigerly1 <tigerlymad@gmail.com>
2019-08-20 13:16:26 +0200

    ok

--- static/lider/SiteL.js
+++ static/lider/SiteL.js
@@ -162,14 +162,13 @@ class SiteL {
         $("#phone").append('<img src="../img/EL.svg" usemap="#image-map"><map name="image-map"><area shape="poly" id="checkBoxClick" title="check" href="#" coords="268,403,133,296,0,202,0,644,2,1331,657,1331,653,702"><area shape="poly" title="number" id="numberClick" href="#" coords="668,696,674,1331,1329,1329,1327,129"><area shape="poly" id="yNClick" title="yesNo" href="#" coords="663,681,2,172,2,2,1331,0,1324,118" ></map>')
         $('map').imageMapResize();
         console.log(document.body)
-        $('#checkBoxClick').on("click", (e) => {
+        $('#yNClick').on("click", (e) => {
             e.preventDefault();
             $("#phone").empty()
             this.LiderSite = "RYN"
             this.tNChoice()
             rooms.yesNoEvent()
         })
-        console.log($('#yNClick'))
         $('#numberClick').on("click", (e) => {
             e.preventDefault();
             $("#phone").empty()

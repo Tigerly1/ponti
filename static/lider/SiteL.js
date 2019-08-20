ea1bbc14c13be2e6463c52b37c712b7f826abaee
Tigerly1 <tigerlymad@gmail.com>
2019-08-20 12:58:30 +0200

    okej

--- static/lider/SiteL.js
+++ static/lider/SiteL.js
@@ -159,10 +159,10 @@ class SiteL {
              if (i == 2) var area = $('<area target="_blank" id="yNClick" alt="yesNo" title="yesNo" href="https://www.facebook.com" coords="663,681,2,172,2,2,1331,0,1324,118" shape="poly">')
              $(map).append(area)
          } */
-        $("#phone").append('<img src="../img/EL.svg" usemap="image-map"><map name="image-map" class="mapChoose"><area target="_blank" alt="check" id="checkBoxClick" title="check" href="" coords="268,403,133,296,0,202,0,644,2,1331,657,1331,653,702" shape="poly"><area target="_blank" alt="number" title="number" id="numberClick" href="" coords="668,696,674,1331,1329,1329,1327,129" shape="poly"><area target="_blank" id="yNClick" alt="yesNo" title="yesNo" href="" coords="663,681,2,172,2,2,1331,0,1324,118" shape="poly"></map>')
+        $("#phone").append('<img src="../img/EL.svg" usemap="#image-map"><map name="image-map"><area shape="poly" id="checkBoxClick" title="check" href="https://www.facebook.com" coords="268,403,133,296,0,202,0,644,2,1331,657,1331,653,702"><area shape="poly" title="number" id="numberClick" href="https://www.facebook.com" coords="668,696,674,1331,1329,1329,1327,129"><area shape="poly" id="yNClick" title="yesNo" href="https://www.facebook.com" coords="663,681,2,172,2,2,1331,0,1324,118" ></map>')
         $('map').imageMapResize();
         console.log(document.body)
-        $('#checkBoxClick').on("click", (e) => {
+        /* $('#checkBoxClick').on("click", (e) => {
             e.preventDefault();
             $("#phone").empty()
             this.LiderSite = "RYN"
@@ -181,7 +181,7 @@ class SiteL {
             $("#phone").empty()
             this.LiderSite = "RCB"
             this.checkbox()
-        })
+        }) */
         var img4 = new Image()
         img4.src = "../img/ST.jpg"
         $(img4).attr('id', 'R4')
